316d39c4eb72ca64d41423059693013fb54781d0
Anatoly Kuzmin <tolaykuzay@gmail.com>
2019-12-23 06:27:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import initStore from './redux';
+import AddTodo from './containers/AddTodo';
 
 const store = initStore();
+store.subscribe(()=>{
+  console.log(`subscribe ${store.getState()}`)
+})
+console.log(store.getState())
 
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
-      <div>Test App</div>
+      <AddTodo />
     </BrowserRouter>
   </Provider>
 );
--- src/redux/modules/list.js
+++ src/redux/modules/list.js
@@ -1,21 +1,62 @@
 // Initial State
 const initialState = {
-  test: false,
+  // "todos": [
+  //   {
+      "id": 42,
+      "created": "2019-12-20T11:50:00.000Z",
+      "name": "Become a Senior Lead Frontend React Architect",
+      "done": false
+  //   },
+  //   {
+  //     "id": 1555,
+  //     "created": "2019-12-19T20:00:00.000Z",
+  //     "name": "Do magic",
+  //     "done": false
+  //   },
+  //   {
+  //     "id": 137,
+  //     "created": "2019-12-11T12:00:00.000Z",
+  //     "name": "Create React App",
+  //     "done": false
+  //   },
+  //   {
+  //     "id": 20,
+  //     "created": "2019-12-15T09:12:00.000Z",
+  //     "name": "Learn JS",
+  //     "done": true
+  //   },
+  //   {
+  //     "id": 215125,
+  //     "created": "2018-12-15T11:48:00.000Z",
+  //     "name": "Learn HTML/CSS",
+  //     "done": true
+  //   }
+  // ]
 };
 
 // Action types
 const actionTypes = {
   SET_TEST: 'SET_TEST',
+  ADD_TASK: 'ADD_TASK',
+  DELETE_TASK: 'DELETE_TASK' 
 };
+
 
 // Reducer
 export default (state = initialState, action = { type: null, payload: null }) => {
   const { type, payload } = action;
+  debugger;
   switch (type) {
     case actionTypes.SET_TEST: {
       return {
         ...state,
         test: payload,
+      };
+    }
+    case actionTypes.ADD_TASK: {
+      return {
+        ...state,
+        name: payload,
       };
     }
     default: {
@@ -25,4 +66,5 @@ export default (state = initialState, action = { type: null, payload: null }) =>
 };
 
 // Actions
-export const setTest = (newTest) => ({ type: actionTypes.SET_TEST, payload: newTest });
+export const setTest = newTest => ({ type: actionTypes.SET_TEST, payload: newTest });
+export const addTodo = newText => ({ type: actionTypes.ADD_TASK, payload: newText });
